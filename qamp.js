3f37484372cc7a515c6633c43fa4929fbb04f3f3
KleiH2021 <95932513+KleiH2021@users.noreply.github.com>
2022-01-17 21:43:57 -0500

    changed instructions

--- qamp.js
+++ qamp.js
@@ -173,6 +173,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 			'We will be showing you pairs of pictures flashed one after the other.  The first picture is a' +
 			' real-life image.  You should do nothing with the real-life image; it is simply a ' +
 			' warning signal that the second image is about to appear. ' +
+			' The second image is a character from an ancient alphabet. Your job is to make a quick judgement on how pleasant each drawing is ' +
 			'Press the key <B>rightKey</B> if the targetCat is more rightAttribute than average. ' + 
 			'Hit the <b>leftKey</b> key if it is more leftAttribute than average.<br/><br/>' + 
 			'The items appear and disappear quickly.  ' + 
